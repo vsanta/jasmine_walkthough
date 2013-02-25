4e367f0c54a4551585e4bdfccd14e07ea1df3aae
vsanta <vanessa.santanna@gmail.com>
2013-02-24 23:41:45 -0500

    Initial app structure

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Jstesting::Application.config.secret_token = '26aaea8d009d2f7e7b5ba4bdee23f2978e0b0db785d6d67a5785c1485caede2a43bf07809d08dbe2eff04cf4b03de42dd60622ebd4677101bf9503933596c6b9'
