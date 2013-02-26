719960c16b9c1ee2062fe137ebec77b78661eb75
vsanta <vanessa.santanna@gmail.com>
2013-02-25 21:46:46 -0500

    Production assets config for heroku

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -9,13 +9,14 @@ Jstesting::Application.configure do
   config.action_controller.perform_caching = true
 
   # Disable Rails's static asset server (Apache or nginx will already do this)
-  config.serve_static_assets = false
+  config.serve_static_assets = true
 
   # Compress JavaScripts and CSS
   config.assets.compress = true
 
   # Don't fallback to assets pipeline if a precompiled asset is missed
-  config.assets.compile = false
+  config.assets.compile = true
+  config.assets.initialize_on_precompile = false
 
   # Generate digests for assets URLs
   config.assets.digest = true
